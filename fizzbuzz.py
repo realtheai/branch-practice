0580771d745dd43564de8e28a2a27f3402166e9c
realtheai <realtheai.insight@gmail.com>
2025-08-22 12:20:44 +0900

    feat: changed

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,6 @@
 for j in range(1,17+1):
-    if j % 3 == 0:
-        print('fizz')
-    elif j % 5 == 0:
-        print('buzz')
+    if j % 15  == 0:
+        print('fizzbuzz')
     else:
         print(j)
 
